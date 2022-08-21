a52c0ce7cad50aeed0e63503863beaa4bab44248
rahulreddybiski <rahulreddy.biski@gmail.com>
2022-08-21 19:02:18 +0530

    completed the project

---
+++ script.js
@@ -0,0 +1,81 @@
+const totalBill = document.querySelector('.total-bill');
+const chooseTipPercentage = document.querySelectorAll('.btn')
+const plus = document.querySelector('#plus')
+const minus = document.querySelector('#minus')
+const persons = document.querySelector('.text-label')
+const perPerson = document.querySelector('#billperperson')
+
+const calculateBill = document.querySelector('.calculatebill')
+let ourValue;
+
+function readUserBill(){
+    const totalBillValue = document.querySelector('#billtotal').value;
+    return totalBillValue;
+    
+}
+
+
+
+// choose tip percentage
+function chooseTip(){
+    chooseTipPercentage.forEach(tipvalue => {
+        tipvalue.addEventListener('click', () => {
+            //console.log(parseInt(tipvalue.innerText))
+            tipvalue.style.backgroundColor = 'salmon'
+            //console.log(parseInt(tipvalue.innerText))
+            ourValue = parseInt(tipvalue.innerText);
+        }) 
+    })
+}
+
+//adding totalmembers
+plus.addEventListener('click',(e) => {
+    let value = parseInt(persons.innerText)
+    //persons.innerText += 1;
+    value += 1;
+    persons.innerText = value.toString()
+})
+
+//subtracting totalmembers
+minus.addEventListener('click',(e) => {
+
+    let value = parseInt(persons.innerText)
+    //persons.innerText += 1;
+    if(value === 0) return
+    value -= 1;
+    persons.innerText = value.toString()
+})
+
+
+// total bill form
+function totalBillAmount(){
+    totalBill.addEventListener('submit', (e) => {
+        e.preventDefault();
+        alert('please click on calculate after filling all details')
+    })
+}
+
+function render(){
+    readUserBill()
+    totalBillAmount()
+    chooseTip() 
+}
+
+render()
+
+calculateBill.addEventListener('click', () => {
+    let perPeron;
+    //chooseTip()
+    let userTotalBill = readUserBill();
+    let tipPercentage = ourValue;
+    let totalPersons = parseInt(persons.innerText);
+
+    let totalTip = (parseInt(userTotalBill) * (tipPercentage/100));
+    console.log(totalTip)
+    let billPerPerson = (parseInt(userTotalBill) + parseInt(totalTip))/totalPersons
+
+    
+    perPerson.value = `${billPerPerson} $`;
+
+
+})
